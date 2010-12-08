7c39d632e36ef52deec6d066e45dc25aa9bf6a55
Steve Conover <sconover@gmail.com>
2010-12-07 17:39:47 -0800

    semicolons, because the rest of the world cares deeply about this kind of thing

--- specs/node_spec.js
+++ specs/node_spec.js
@@ -1,25 +1,25 @@
-require("./spec_helper.js")
+require("./spec_helper.js");
 
 describe("Jaml.Node", function() {
   
   beforeEach(function(){
-    fooBar = new Jaml.Node("fooBar")
-  })
+    fooBar = new Jaml.Node("fooBar");
+  });
   
   describe("basics", function() {
     it("renders nothing inside if there's nothing in the node", function(){
-      assert.equal("<fooBar></fooBar>\n", new Jaml.Node("fooBar").render())
-    })  
+      assert.equal("<fooBar></fooBar>\n", new Jaml.Node("fooBar").render());
+    });
 
     it("properly prepends the specified amount of left padding", function(){
-      assert.equal("     <fooBar></fooBar>\n", new Jaml.Node("fooBar").render(5))
-    })  
-  })
+      assert.equal("     <fooBar></fooBar>\n", new Jaml.Node("fooBar").render(5));
+    });
+  });
   
   describe("attributes", function() {
     it("renders attributes as key value pairs separate by = in the tag", function(){
-      assert.equal("<fooBar a=\"b\"></fooBar>\n", fooBar.setAttributes({a:"b"}).render())
-    })  
-  })
-})
+      assert.equal("<fooBar a=\"b\"></fooBar>\n", fooBar.setAttributes({a:"b"}).render());
+    });
+  });
+});
 
