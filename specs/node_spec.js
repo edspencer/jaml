bee94567ee7ebe77baafec326e8714a53b0a42f6
Steve Conover <sconover@gmail.com>
2010-12-07 17:34:52 -0800

    test padding

--- specs/node_spec.js
+++ specs/node_spec.js
@@ -4,5 +4,9 @@ describe("Jaml.Node", function() {
   it("renders nothing inside if there's nothing in the node", function(){
     assert.equal("<fooBar></fooBar>\n", new Jaml.Node("fooBar").render())
   })  
+
+  it("properly prepends the specified amount of left padding", function(){
+    assert.equal("     <fooBar></fooBar>\n", new Jaml.Node("fooBar").render(5))
+  })  
 })
 
